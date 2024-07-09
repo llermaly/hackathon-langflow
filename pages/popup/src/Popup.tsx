a981c2e2a4f06ac0a50469ced12cf535afc31b2e
Jeffrey <jeffrey.rengifom@gmail.com>
2024-07-09 14:06:21 -0500

    Now the app inserts the text to the form

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -27,22 +27,78 @@ export function getBodyHTML() {
   return document.body.innerHTML;
 }
 
+export function modifyInputValues(data: string) {
+  let localData = null;
+  try {
+    localData = JSON.parse(data);
+  } catch (e) {
+    console.error('Error parsing json: ', e);
+    throw new Error('Error parsing json: ', e as Error);
+  }
+
+  const jsonData = JSON.parse(localData);
+
+  // Select all elements with jsname attribute: this is used to clear the initial text in the input fields
+  const elementsWithJsName = document.querySelectorAll('div[jsname="LwH6nd"]');
+
+  elementsWithJsName.forEach(element => {
+    element.textContent = '';
+  });
+
+  // Loop through the JSON data and set the values in the input fields
+  Object.keys(jsonData).forEach(key => {
+    const { HTMLselectorType, HTMLselectorValue, value } = jsonData[key];
+
+    let selector;
+
+    if (HTMLselectorType === 'id') {
+      selector = `#${HTMLselectorValue}`;
+    } else if (HTMLselectorType === 'class') {
+      selector = `.${HTMLselectorValue}`;
+    } else if (HTMLselectorType === 'aria-labelledby') {
+      selector = `input[aria-labelledby="${HTMLselectorValue}"]`;
+    } else {
+      console.error('Unsupported selector type: ', HTMLselectorType);
+      return;
+    }
+
+    const element = document.querySelector(selector);
+
+    // Set the value in the input field
+    if (element) {
+      if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+        element.value = value;
+      } else {
+        if (Array.isArray(value)) {
+          element.innerHTML = value.map(item => JSON.stringify(item, null, 2)).join('<br>');
+        } else if (typeof value === 'object') {
+          element.innerHTML = JSON.stringify(value, null, 2);
+        } else {
+          element.innerHTML = value;
+        }
+      }
+    } else {
+      console.error('Element not found for selector:', selector);
+    }
+  });
+}
+
 export function getFormHTML() {
   const form = document.querySelector('form');
-  return form ? form.outerHTML : 'No se encontró ningún formulario';
+  return form ? form.outerHTML : 'No form found in the page.';
+}
+
+function saveToLocalStorage(data: string, key: string) {
+  try {
+    localStorage.setItem(key, data);
+  } catch (e) {
+    throw new Error('Error saving to local storage: ', e as Error);
+  }
 }
 
 const EcommerceStep = () => {
   const [savedData, setSavedData] = useState<string>(JSON.stringify({ data: 'something' }));
   const [html, setHtml] = useState<string>('');
-
-  const handleSaveToLocalStorage = (data: string) => {
-    try {
-      localStorage.setItem('e-commerce-info', data);
-    } catch (e) {
-      throw new Error('Error saving to local storage: ', e as Error);
-    }
-  };
 
   const sendHtmlToLangflow = async () => {
     const response = await postEcommerceHtmlParserFlow(html);
@@ -50,7 +106,7 @@ const EcommerceStep = () => {
     try {
       setSavedData(JSON.stringify(response));
 
-      handleSaveToLocalStorage(JSON.stringify(response));
+      saveToLocalStorage(JSON.stringify(response), 'e-commerce-info');
     } catch (e) {
       console.error('Error parsing json: ', e);
       throw new Error('Error parsing json: ', e as Error);
@@ -81,32 +137,29 @@ const EcommerceStep = () => {
       </div>
       <Button onClick={sendHtmlToLangflow}>Send html to langflow</Button>
       <div>
-        <p className="font-bold">Obtained data from langflow:</p> <span>{savedData}</span>
+        <p className="font-bold">Obtained data from Langflow:</p> <span>{savedData}</span>
       </div>
     </div>
   );
 };
 
 const FormStep = () => {
-  const [savedData, setSavedData] = useState<string>(localStorage.getItem('e-commerce-info') ?? '');
+  const [savedData, setSavedData] = useState<string>(localStorage.getItem('form-info') ?? '');
   const [html, setHtml] = useState<string>('');
 
-  const handleSaveToLocalStorage = (data: string) => {
-    try {
-      localStorage.setItem('form-info', data);
-    } catch (e) {
-      throw new Error('Error saving to local storage: ', e as Error);
-    }
-  };
-
   const getLangflowData = async () => {
-    console.log('saved: ', savedData);
-    const response = await postFormHtmlParserFlow(html, savedData);
+    const ecommerceData = localStorage.getItem('e-commerce-info');
+
+    if (!ecommerceData) {
+      throw new Error('No e-commerce data found in local storage');
+    }
+
+    const response = await postFormHtmlParserFlow(html, ecommerceData);
 
     try {
       setSavedData(JSON.stringify(response));
 
-      handleSaveToLocalStorage(JSON.stringify(response));
+      saveToLocalStorage(JSON.stringify(response), 'form-info');
     } catch (e) {
       console.error('Error parsing json: ', e);
       throw new Error('Error parsing json: ', e as Error);
@@ -128,6 +181,26 @@ const FormStep = () => {
     });
   };
 
+  const insertData = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id || 0 },
+          func: modifyInputValues,
+          args: [savedData], //Args to pass to the function
+        },
+        results => {
+          console.log(results[0].result);
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+          } else {
+            console.log('Script injected successfully');
+          }
+        },
+      );
+    });
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <div>
@@ -140,6 +213,7 @@ const FormStep = () => {
         <div className="max-h-[200px] max-w-[300px] overflow-auto">{html}</div>
       </div>
       <Button onClick={getLangflowData}>Send form to fill + product data</Button>
+      <Button onClick={insertData}>Insert data</Button>
       <div>
         <p>Obtained data from langflow:</p> <span>{savedData}</span>
       </div>
